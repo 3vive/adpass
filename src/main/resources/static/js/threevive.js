34510c6e8a84043fc9c6ddc9da37bb76a7008768
Ryan Urtheil <ryan.urtheil@3vive.com>
2018-11-29 14:49:55 -0500

    CSS Update 112918

--- src/main/resources/static/js/threevive.js
+++ src/main/resources/static/js/threevive.js
@@ -14,28 +14,16 @@ if (location.hostname === "localhost") {
   function threeVive() {
     var _threeViveObject = {};
     var cssId = 'myCss';
-<<<<<<< HEAD
     if (!document.getElementById(cssId)) {
       var head = document.getElementsByTagName('head')[0];
       var link = document.createElement('link');
       link.id = cssId;
       link.rel = 'stylesheet';
       link.type = 'text/css';
-      link.href = 'https://raw.githack.com/3vive/adpass/master/src/main/resources/static/css/threevive.css';
+      link.href = 'css/threevive.css';
+      //link.href = 'https://raw.githack.com/3vive/adpass/master/src/main/resources/static/css/threevive.css';
       link.media = 'all';
       head.appendChild(link);
-=======
-    if (!document.getElementById(cssId))
-    {
-        var head  = document.getElementsByTagName('head')[0];
-        var link  = document.createElement('link');
-        link.id   = cssId;
-        link.rel  = 'stylesheet';
-        link.type = 'text/css';
-        link.href = 'https://raw.githack.com/3vive/adpass/master/src/main/resources/static/css/threevive.css';
-        link.media = 'all';
-        head.appendChild(link);
->>>>>>> 09af388cc769f9bdfad8e96aff54b248587424d3
     }
     var currentACBal;
     //Generate the iframe in parent window
@@ -72,7 +60,9 @@ if (location.hostname === "localhost") {
     _threeViveObject.generateButtons = function() {
       var inputStyles = "background:none;border-color:#888;border-width:0 0 1px 0;width:100%;color:#fff;padding:5px;margin:5px;",
         btnStyles = "background:#129979;border:none;width:60%;color:#fff;padding:5px;margin:5px;border-radius: 15px;",
+        btnStylesWallet = "background:#129979;border:none;color:#fff;padding:5px;margin:0px;border-radius: 15px;width:80% !important;",
         btnStylesInverse = "background:white;border:2px solid #129979;width:60%;color:#129979;padding:3px;margin:5px;border-radius: 15px",
+        btnStylesAccount = "background:#129979;border:2px solid #129979;width:60%;color:#fff;padding:3px;margin:5px;",
         forgetStyles = "color:#fff;",
         startYears = 10,
         endYears = 70,
@@ -80,14 +70,19 @@ if (location.hostname === "localhost") {
       var buttonDiv = document.createElement('div');
       buttonDiv.id = "ViveButtons";
       buttonDiv.style.textAlign = "center";
-      buttonDiv.innerHTML = "<div style='border-top:1px solid #129979;border-right: 1px solid #129979;border-left: 1px solid #129979;margin:auto;width:90%;height:23px'><div style='color:#129979;background-color:#fff;width:100px;margin: -14px auto'>ADPASS</div></div>" + "<input type='button' onclick='threeVive.scrollTrigger();' value='Login' style='" + btnStyles + "' />" + "<input type='button' onclick='threeVive.registerButton();' value='Registration' style='" + btnStyles + "' />" + "<input type='button' onclick='threeVive.loadAllAdRevenue();' value='Free with ADs' style='" + btnStylesInverse + "' />" + "<div style='border-bottom:1px solid #129979;border-right: 1px solid #129979;border-left: 1px solid #129979;margin:auto;width:90%;height:23px;clear:both;margin-bottom:30px'></div>";
+      buttonDiv.innerHTML = "<div style='border-top:1px solid #129979;border-right: 1px solid #129979;border-left: 1px solid #129979;margin:auto;width:90%;height:23px'><div style='color:#129979;background-color:#fff;width:100px;margin: -14px auto;border: solid 1px;border-radius: 15px;'>ADPASS</div></div>" + 
+                              "<input type='button' onclick='threeVive.scrollTrigger();' value='Login' style='" + btnStyles + "' class='inputButton' />" + 
+                              "<input type='button' onclick='threeVive.registerButton();' value='Registration' style='" + btnStyles + "' class='inputButton' />" + 
+                              "<input type='button' onclick='threeVive.loadAllAdRevenue();' value='Free with ADs' style='" + btnStylesInverse + "' />" + 
+                              //"<div id='walletButCont'><input type='button' id='walletBut' onclick='threeVive.loadAllAdRevenue();' value='Pay using ADPASS' style='" + btnStylesWallet + "' />my wallet</div>" + 
+                            "<div id='borderBottom' style='margin:auto;width:90%;height:23px;clear:both'>Continue reading content by clicking any of the options above. Using ADPASS is a quick and easy way to avoid the hassle of Advertising and Tracking while maintaining your privacy.</div>";
       //debugger;
       var whereToAppendButtons = document.getElementsByClassName("paywallButtons")[0];
       whereToAppendButtons.appendChild(buttonDiv);
     }
     _threeViveObject.registerButton = function() {
       // create the elements
-      var 
+      var
         centeredDiv = document.createElement('div'),
         div = document.createElement('div'),
         registrationForm = document.createElement('form');
@@ -99,8 +94,8 @@ if (location.hostname === "localhost") {
       //document.body.style.background = "url('https://cdn.lennar.net/images/com/images/new-homes/3/63/mhi/El%20Dorado%20Hills%20Sunset-1200x540.jpg?w=1200&h=540&as=1 no-repeat";
       //document.body.style.backgroundSize = "cover";
       centeredDiv.style.padding = '10px';
-      centeredDiv.style.position = 'fixed';
-      centeredDiv.style.zIndex = 999;
+      //centeredDiv.style.position = 'fixed';
+      //centeredDiv.style.zIndex = 999;
       centeredDiv.style.left = 0;
       centeredDiv.style.top = 0;
       centeredDiv.style.width = '100%';
@@ -110,7 +105,7 @@ if (location.hostname === "localhost") {
       // set main-div styles
       div.style.background = "#129979";
       div.style.width = '300px';
-      div.style.margin = '30px auto';
+      div.style.margin = '0px auto';
       div.style.padding = '10px';
       div.style.borderRadius = '10px';
       div.style.borderTop = "0px #129979 solid";
@@ -120,13 +115,13 @@ if (location.hostname === "localhost") {
       //  div.style.position = 'fixed';
       div.style.left = 0;
       div.style.top = 0;
-      div.style.width = '40%';
+      div.style.width = '100%';
       div.style.overflow = 'auto';
       div.style.paddingBottom = '20px';
       div.innerHTML = '<button type="button" id="closeButton" onclick="threeVive.closePopup()" style="background-color:#129979; margin-left:80%;width:8%;"> X</button> <h1 style="text-align: center;color:white;font-family: Helvetica Neue, sans-serif;font-weight: bold;letter-spacing: -1px;font-style: italic; font-size: 28px; padding-bottom:0;margin-top:-32px"><img src="https://image.flaticon.com/icons/svg/74/74474.svg" width="30px" height="30px"> Ad Pass</h1>';
       // hide login form and show register form
       // create some variables for styling
-      var inputStyles = "background:#fff;border: 1px solid white;width:90%;color:#fff;padding:5px;margin:5px;",
+      var inputStyles = "background:#fff;border: 1px solid white;width:90%;color:#000;padding:5px;margin:5px;",
         btnStyles = "background:#fff;border:none;width:80%;color:#129979;padding:5px;margin:15px 5px 5px 5px;border-radius: 15px",
         forgetStyles = "color:#fff;",
         labelStyles = "color: white; width: 100%;text-align: left;",
@@ -137,8 +132,13 @@ if (location.hostname === "localhost") {
       //    leftLoginDiv.style.borderRight = "2px #129979 solid";
       div.appendChild(registrationForm);
       centeredDiv.appendChild(div);
-      // append main-div on the body
-      document.body.appendChild(centeredDiv);
+      // append main-div to primary column
+      document.getElementById("borderBottom").appendChild(centeredDiv);
+      document.getElementById("borderBottom").style.height = "auto";
+      var elems = document.getElementsByClassName("inputButton");
+          for(var i = 0; i < elems.length; i++) {
+            elems[i].disabled = true;
+        }
       //  window.removeEventListener("scroll", _threeViveObject.scrollTrigger);
     }
     _threeViveObject.scrollTrigger = function() {
@@ -150,6 +150,7 @@ if (location.hostname === "localhost") {
           rightLoginDiv = document.createElement('div'),
           loginForm = document.createElement('form'),
           registrationForm = document.createElement('form');
+          centeredDiv.id = "mContainer";
         //  registerForm
         // set body styles
         //  document.body.style.color = '#fff';
@@ -157,8 +158,8 @@ if (location.hostname === "localhost") {
         //document.body.style.background = "url('https://cdn.lennar.net/images/com/images/new-homes/3/63/mhi/El%20Dorado%20Hills%20Sunset-1200x540.jpg?w=1200&h=540&as=1 no-repeat";
         //document.body.style.backgroundSize = "cover";
         centeredDiv.style.padding = '10px';
-        centeredDiv.style.position = 'fixed';
-        centeredDiv.style.zIndex = 999;
+        //centeredDiv.style.position = 'fixed';
+        //centeredDiv.style.zIndex = 999;
         centeredDiv.style.left = 0;
         centeredDiv.style.top = 0;
         centeredDiv.style.width = '100%';
@@ -168,7 +169,7 @@ if (location.hostname === "localhost") {
         // set main-div styles
         div.style.background = "#129979";
         div.style.width = '300px';
-        div.style.margin = '30px auto';
+        div.style.margin = '0px auto';
         div.style.padding = '10px';
         div.style.borderRadius = '10px';
         div.style.borderTop = "0px #129979 solid";
@@ -178,7 +179,7 @@ if (location.hostname === "localhost") {
         //  div.style.position = 'fixed';
         div.style.left = 0;
         div.style.top = 0;
-        div.style.width = '40%';
+        div.style.width = '100%';
         div.style.overflow = 'auto';
         div.innerHTML = '<button type="button" id="closeButton" onclick="threeVive.closePopup()" style="background-color:#129979; margin-left:80%;width:8%;"> X</button> <h1 style="text-align: center;color:white;font-family: Helvetica Neue, sans-serif;font-weight: bold;letter-spacing: -1px;font-style: italic; font-size: 28px; padding-bottom:0;margin-top:-32px"><img src="https://image.flaticon.com/icons/svg/74/74474.svg" width="30px" height="30px"> Ad Pass</h1>';
         // hide login form and show register form
@@ -216,19 +217,32 @@ if (location.hostname === "localhost") {
         }
         centeredDiv.appendChild(div);
         // append main-div on the body
-        document.body.appendChild(centeredDiv);
+        document.getElementById("borderBottom").appendChild(centeredDiv);
+        document.getElementById("borderBottom").style.height = "auto";
+        var elems = document.getElementsByClassName("inputButton");
+          for(var i = 0; i < elems.length; i++) {
+            elems[i].disabled = true;
+        }
         window.removeEventListener("scroll", _threeViveObject.scrollTrigger);
       }
     }
     _threeViveObject.scrollUntilPopup = function() {
       window.addEventListener('scroll', _threeViveObject.scrollTrigger)
     }
+
+   
+
     _threeViveObject.loadAllAdRevenue = function() {
       var myEleToUnBlur = document.getElementsByClassName('paywallTrunk');
       myEleToUnBlur[0].style.color = null;
       var value = null;
       myEleToUnBlur[0].style.textShadow = value;
       document.getElementById('ViveButtons').style.display = "none";
+      var adElems = document.getElementsByClassName("advertisement-content");
+        for(var i = 0; i < adElems.length; i++) {
+            adElems[i].style.display = "block";
+        };
+        // Make some sort of DFP Ad refresh request here
     };
     _threeViveObject.load3ViveModule = function() {};
     _threeViveObject.hideArticleContent = function() {};
@@ -238,6 +252,11 @@ if (location.hostname === "localhost") {
     _threeViveObject.closePopup = function() {
       var element = document.getElementById('mContainer');
       element.parentNode.removeChild(element);
+      document.getElementById("borderBottom").style.height = "23px";
+      var elems = document.getElementsByClassName("inputButton");
+          for(var i = 0; i < elems.length; i++) {
+            elems[i].disabled = false;
+        }
     }
     _threeViveObject.validateForm = function() {
       var usr = document.getElementById('adPassUserNameReg');
@@ -325,7 +344,7 @@ if (location.hostname === "localhost") {
       }).then(function(newUser) {
         console.log(newUser);
         debugger;
-        if (newUser.enabled = true) { //account creation is true then send over the account
+        if (newUser.enabled == true || newUser.enabled == "true") { //account creation is true then send over the account
           alert("Placeholder: Thank you for registering, We've added a complimentary $" + newUser.userAccount.accountBalance + "to you wallet. Use ADpass dollars on any sites where we are partnered with and enjoy an ad free experience")
           currentACBal = newUser.userAccount.accountBalance;
           var thishtml = '<div id="snackbar">' + currentACBal + '</div>'
